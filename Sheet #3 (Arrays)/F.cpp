91ad6d7edb4657b7b8d831aa1e04f21ff9f909e9
ishratpayel <164357630+ishratpayel@users.noreply.github.com>
2025-06-01 21:06:53 +0600

    Add files via upload

---
+++ Sheet #3 (Arrays)/F.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
